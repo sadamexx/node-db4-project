43996931327c0ab52a422bc4d4bd0c5dc018ad78
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-02-20 22:32:02 -0800

    added ingredient router

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const recipeRouter = require('./recipes/recipeRouter.js');
+const ingredientRouter = require('./ingredients/ingredientRouter.js');
 
 const server = express();
 server.use(express.json());
 
 server.use('/api/recipes', recipeRouter);
+server.use('/api/ingredients', ingredientRouter);
 
 server.get('/', (req, res) => {
     res.send('<h2> We be Migratin and Seeding in this beezy </h2>');
